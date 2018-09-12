574320e4add0520c6cd186db84abb522329a0482
Yaroslav Chizh <chiyar@mail.ru>
2018-09-13 01:35:14 +0300

    Added extra tests

--- task1/test.cpp
+++ task1/test.cpp
@@ -1,14 +1,24 @@
+#include <cassert>
+#include <string>
+
 #include "solver.hpp"
 
-void simple_test() {
-    double a = 1, b = -2, c = 1;
-    Answer answer{2, 1, TWO_ROOTS};
-
+void test(const std::string& testName, double a, double b, double c, const Answer& answer) {
+    printf("--%s\n", testName.data());
     SquareTrinomialSolver solver;
     assert(answer == solver.SolveSquareTrinomial(a, b, c));
+    printf("--Test passed!\n\n");
+}
+
+void run_tests() {
+    test("one_root_test", 0, 4.2, 5.3, Answer{-5.3 / 4.2, 0, ONE_ROOT});
+    test("two_roots_test", 1, -2, 1, Answer{1, 1, TWO_ROOTS});
+    test("no_roots_test", 2.1, 2.67, 1.04, Answer{0, 0, NO_ROOTS});
+    test("infinite_number_of_roots_test", 0, 0, 0, Answer{0, 0, INF_ROOTS});
 }
 
 int main() {
-    simple_test();
+    run_tests();
+    printf("All tests passed!\n");
     return 0;
 }
