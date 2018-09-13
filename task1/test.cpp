fd7310dc31485216be1e4dc5f09f8bb96000dd9f
Yaroslav Chizh <chiyar@mail.ru>
2018-09-13 11:31:07 +0300

    Changed functions' style

--- task1/test.cpp
+++ task1/test.cpp
@@ -3,22 +3,22 @@
 
 #include "solver.hpp"
 
-void test(const std::string& testName, double a, double b, double c, const Answer& answer) {
+void Test(const std::string& testName, double a, double b, double c, const Answer& answer) {
     printf("--%s\n", testName.data());
     SquareTrinomialSolver solver;
     assert(answer == solver.SolveSquareTrinomial(a, b, c));
     printf("--Test passed!\n\n");
 }
 
-void run_tests() {
-    test("one_root_test", 0, 4.2, 5.3, Answer{-5.3 / 4.2, 0, ONE_ROOT});
-    test("two_roots_test", 1, -2, 1, Answer{1, 1, TWO_ROOTS});
-    test("no_roots_test", 2.1, 2.67, 1.04, Answer{0, 0, NO_ROOTS});
-    test("infinite_number_of_roots_test", 0, 0, 0, Answer{0, 0, INF_ROOTS});
+void RunTests() {
+    Test("one_root_test", 0, 4.2, 5.3, Answer{-5.3 / 4.2, 0, ONE_ROOT});
+    Test("two_roots_test", 1, -2, 1, Answer{1, 1, TWO_ROOTS});
+    Test("no_roots_test", 2.1, 2.67, 1.04, Answer{0, 0, NO_ROOTS});
+    Test("infinite_number_of_roots_test", 0, 0, 0, Answer{0, 0, INF_ROOTS});
 }
 
 int main() {
-    run_tests();
+    RunTests();
     printf("All tests passed!\n");
     return 0;
 }
