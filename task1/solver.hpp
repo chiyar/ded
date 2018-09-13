bd6f3b6a411650e73008970d2501f604cc6545fd
Yaroslav Chizh <chiyar@mail.ru>
2018-09-13 03:04:58 +0300

    Added docs

--- task1/solver.hpp
+++ task1/solver.hpp
@@ -21,12 +21,43 @@ struct Answer {
     }
 }; 
 
+//! Provides an interface to work with square trinomial
 class SquareTrinomialSolver{
 public:
+    //! Finds roots of square trinomial ax2 + bx + c
+    //!
+    //! @param [in] a a-coefficient
+    //! @param [in] b b-coefficient
+    //! @param [in] c c-coefficient
+    //!
+    //! @return Roots and number of them
+    //!
+    //! @note In case of no roots or infinity number of roots
+    //!       returns NO_ROOTS or INF_ROOTS flag respectively
     static Answer SolveSquareTrinomial(double a, double b, double c);
+
+    //! Prints roots of square trinomial
+    //! @param answer 
     static void PrintAnswer(const Answer& answer);
 
 private:
+    //! Finds discriminant of square trinomial ax2 + bx + c
+    //! 
+    //! @param [in] a a-coefficient
+    //! @param [in] b b-coefficient
+    //! @param [in] c c-coefficient
+    //!
+    //! @return Discriminant of square trinomial
     static double FindDiscriminant(double a, double b, double c);
+
+    //! Find root of linear function kx + b
+    //!
+    //! @param [in] k k-coefficient
+    //! @param [in] b b-coefficient
+    //!
+    //! @return Root of linear function
+    //!
+    //! @note In case of no roots or infinity number of roots
+    //!       returns NO_ROOTS or INF_ROOTS flag respectively
     static Answer SolveLinearEquation(double k, double b);
 };
